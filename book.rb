a97b076c0a7595f5c9fedb7c0cee37e0e3f14cc1
Amaka Konwea <lawrahcollections@gmail.com>
2023-04-04 21:51:38 +0100

    Create a Label class with an association to the Item class

--- book.rb
+++ book.rb
@@ -1,10 +1,9 @@
 require_relative 'item'
 
 class Book < Item
+  attr_accessor :id, :publisher, :cover_state
 
-  attr_accessor :id = Random.rand(1..1000), :publisher, :cover_state
-
-  def initialize (id, publisher, cover_state)
+  def initialize(publisher, cover_state, id = Random.rand(1..1000))
     super(publish_date)
     @id = id
     @publisher = publisher
