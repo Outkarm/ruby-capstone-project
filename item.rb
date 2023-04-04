70c2603134421683538e23d65c37f8c364106778
AmanWorku <amexworku2014@gmail.com>
2023-04-04 16:57:19 +0300

    Create can_be_archived? method in main.rb

--- item.rb
+++ item.rb
@@ -6,10 +6,25 @@ class Item
   def initialize(*)
     @id = Random.rand(1..1000)
     @published_date = nil
-    @archived: false
+    @archived = false
     @label = nil
     @genre = nil
     @author = nil
     @source = nil
+  end
+
+  private
+
+  def can_be_archived?
+    begin
+      publish_date = Date.parse(@publish_date)
+    rescue StandardError
+      publish_date = nil
+    end
+    if publish_date.nil?
+      false
+    else
+      (Date.today.year - publish_date.year) > 10
+    end
   end
 end
