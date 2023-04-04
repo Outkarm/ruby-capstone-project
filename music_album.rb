311e58c7151952de16ac319ae5ea28b671e6240d
AmanWorku <amexworku2014@gmail.com>
2023-04-04 17:24:09 +0300

    Initialize the properties in the constructor

--- music_album.rb
+++ music_album.rb
@@ -5,7 +5,11 @@ class MusicAlbum < Item
     attr_accessor :name, :genres, :publish_date, :on_spotify
     attr_accessor :genre
 
-    def initialize(name)
-        
+    def initialize(name, publish_date, on_spotify: false, genres: [])
+        super()
+        @name = name
+        @publish_date = Date.parse(publish_date)
+        @on_spotify = on_spotify
+        @genres = genres
     end
 end
