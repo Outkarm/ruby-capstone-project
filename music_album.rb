240d0b8ddd5883e460125f1279d2c79f0df1e7f0
AmanWorku <amexworku2014@gmail.com>
2023-04-04 17:29:39 +0300

    Fix all linter errors

--- music_album.rb
+++ music_album.rb
@@ -1,15 +1,13 @@
 require_relative 'item'
 
 class MusicAlbum < Item
+  attr_accessor :name, :genres, :publish_date, :on_spotify, :genre
 
-    attr_accessor :name, :genres, :publish_date, :on_spotify
-    attr_accessor :genre
-
-    def initialize(name, publish_date, on_spotify: false, genres: [])
-        super()
-        @name = name
-        @publish_date = Date.parse(publish_date)
-        @on_spotify = on_spotify
-        @genres = genres
-    end
+  def initialize(name, publish_date, on_spotify: false, genres: [])
+    super()
+    @name = name
+    @publish_date = Date.parse(publish_date)
+    @on_spotify = on_spotify
+    @genres = genres
+  end
 end
