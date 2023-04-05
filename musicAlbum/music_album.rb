4eb6be5ac007c3675363e6066111bf634c1680dc
AmanWorku <amexworku2014@gmail.com>
2023-04-05 14:21:03 +0300

    Add to string function in music_album.rb

--- musicAlbum/music_album.rb
+++ musicAlbum/music_album.rb
@@ -14,4 +14,9 @@ class MusicAlbum < Item
   def can_be_archived?
     super && @on_spotify
   end
+
+  def to_s
+    "<#{self.class} ID: #{@id}> " \
+      "[name: #{@name}, publish_date: #{@publish_date}, on_spotify: #{@on_spotify}]"
+  end
 end
