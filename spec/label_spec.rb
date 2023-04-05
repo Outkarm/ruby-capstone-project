ae28a6837bff29ebba2be6de45227687e5001f2f
Amaka Konwea <lawrahcollections@gmail.com>
2023-04-05 15:40:40 +0100

    Fix error

--- spec/label_spec.rb
+++ spec/label_spec.rb
@@ -11,7 +11,7 @@ describe Label do
       expect(label.title).to eq ' Book-label'
     end
 
-    it 'Should validate the label title' do
+    it 'Should validate all the label title' do
       expect(label.color).to eq 'white'
     end
 
