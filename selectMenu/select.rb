2b01b792dae003a6eb0cda5b41eb5ef77d5e1b53
Outkarm <jkpordje@gmail.com>
2023-04-05 14:02:57 +0000

    reduce length of opt_select method

--- selectMenu/select.rb
+++ selectMenu/select.rb
@@ -5,35 +5,20 @@ class Select
 
   def opt_select(opt)
     case opt
-    when 1
-      @app.add_book
-    when 2
-      @app.add_music_album
-    when 3
-      @app.add_movie
-    when 4
-      @app.add_game
-    when 5
-      @app.list_all_books
-    when 6
-      @app.list_all_music_albums
-    when 7
-      @app.list_all_movies
-    when 8
-      @app.list_all_games
-    when 9
-      @app.list_all_genres
-    when 10
-      @app.list_all_labels
-    when 11
-      @app.list_all_authors
-    when 12
-      @app.list_all_sources
-    when 13
-      puts 'Thanks for using this app'
-      exit
-    else
-      puts 'Option entered is invalid'
+    when 1 then @app.add_book
+    when 2 then @app.add_music_album
+    when 3 then @app.add_movie
+    when 4 then @app.add_game
+    when 5 then @app.list_all_books
+    when 6 then @app.list_all_music_albums
+    when 7 then @app.list_all_movies
+    when 8 then @app.list_all_games
+    when 9 then @app.list_all_genres
+    when 10 then @app.list_all_labels
+    when 11 then @app.list_all_authors
+    when 12 then @app.list_all_sources
+    when 13 then exit puts 'Thanks for using this app'
+    else puts 'Option entered is invalid'
     end
   end
 end
