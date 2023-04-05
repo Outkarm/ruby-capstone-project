b117ec88d1ef1728b615c7f7a53962a467f4f9b1
AmanWorku <amexworku2014@gmail.com>
2023-04-05 12:53:51 +0300

    Fix linter errors

--- spec/music_album_spec.rb
+++ spec/music_album_spec.rb
@@ -3,15 +3,15 @@ require_relative '../item'
 require_relative '../musicAlbum/music_album'
 
 describe MusicAlbum do
-    before(:all) do
-        @music = MusicAlbum.new('Earth Wind and Fire', '1999-12-10', true)
+  before(:all) do
+    @music = MusicAlbum.new('Earth Wind and Fire', '1999-12-10', true)
+  end
+  describe '#can_be_archived?' do
+    it 'exists' do
+      expect(@music).to respond_to(:can_be_archived?)
     end
-    describe '#can_be_archived?' do
-        it 'exists' do
-            expect(@music).to respond_to(:can_be_archived?)
-        end
-        it 'returns false' do
-            expect(@music.can_be_archived?).to be_truthy
-        end
+    it 'returns false' do
+      expect(@music.can_be_archived?).to be_truthy
     end
+  end
 end
