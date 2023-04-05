3b0bd7dc213bc90e7b56e6e224042dbd09bd6076
Amaka Konwea <lawrahcollections@gmail.com>
2023-04-05 15:35:58 +0100

    Fix linter errors

--- selectMenu/select.rb
+++ selectMenu/select.rb
@@ -1,31 +1,70 @@
 class Select
+  OPTIONS = {
+    1 => :add_book,
+    2 => :add_music_album,
+    3 => :add_movie,
+    4 => :add_game,
+    5 => :list_all_books,
+    6 => :list_all_music_albums,
+    7 => :list_all_genres,
+    8 => :list_all_movies,
+    9 => :list_all_games,
+    0 => :exit_app
+  }.freeze
+
   def initialize
     @app = nil
   end
 
   def opt_select(opt)
-    case opt
-    when 1
-      @app.add_book
-    when 2
-      @app.add_music_album
-    when 3
-      @app.add_movie
-    when 4
-      @app.add_game
-    when 5
-      @app.list_all_books
-    when 6
-      @app.list_all_music_albums
-    when 7
-      @app.list_all_movies
-    when 8
-      @app.list_all_games
-    when 9
-      puts 'Thanks for using this app'
-      exit
-    else
+    action = OPTIONS[opt]
+    if action.nil?
       puts 'Option entered is invalid'
+    else
+      send(action)
     end
+  end
+
+  private
+
+  def add_book
+    @app.add_book
+  end
+
+  def add_music_album
+    @app.add_music_album
+  end
+
+  def add_movie
+    @app.add_movie
+  end
+
+  def add_game
+    @app.add_game
+  end
+
+  def list_all_books
+    @app.list_all_books
+  end
+
+  def list_all_music_albums
+    @app.list_all_music_albums
+  end
+
+  def list_all_genres
+    @app.list_all_genres
+  end
+
+  def list_all_movies
+    @app.list_all_movies
+  end
+
+  def list_all_games
+    @app.list_all_games
+  end
+
+  def exit_app
+    puts 'Thanks for using this app'
+    exit
   end
 end
