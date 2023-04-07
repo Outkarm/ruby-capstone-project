c0924341b8d4d7e1e17c1c44495dfecace4cad34
Amaka Konwea <lawrahcollections@gmail.com>
2023-04-07 21:35:04 +0100

    Create a schema.sql file with tables for Book

--- schema/schema.sql
+++ schema/schema.sql
@@ -1,5 +1,14 @@
 CREATE DATABASE catalog
 
+CREATE TABLE items (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    genre_id INT REFERENCES genres(id),
+    author_id INT REFERENCES authors(id),
+    label_id INT REFERENCES labels(id),
+    publish_date DATE NOT NULL,
+    archived BOOLEAN NOT NULL
+)
+
 CREATE TABLE music_albums (
     id INT GENERATED ALWAYS AS IDENTITY,
     on_spotify BOOLEAN,
@@ -17,4 +26,12 @@ CREATE TABLE labels (
     title VARCHAR(100),
     color VARCHAR(100),
     PRIMARY KEY(id)
+);
+
+CREATE TABLE book (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  publish_date DATE,
+  publisher VARCHAR(100),
+  cover_state VARCHAR(100),
+  label_id INT REFERENCES labels(id)
 );
