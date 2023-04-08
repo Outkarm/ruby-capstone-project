f6efbbd8b879f5377577cc87bc123f78e142402a
AmanWorku <amexworku2014@gmail.com>
2023-04-08 22:53:07 +0300

    Fix menu_list.rb

--- selectMenu/menu_list.rb
+++ selectMenu/menu_list.rb
@@ -1,12 +1,12 @@
 def menu_list
   puts 'CATALOG OF OUT THINGS'
   puts '--------------------------------------------------------'
-  puts '1 - Add a new book          7 - List all movies'
-  puts '2 - Add a new music album   8 - List of games'
-  puts '3 - Add a new movie         9 - List of genres'
-  puts '4 - Add a new game          10 - List all labels'
-  puts '5 - List all books          11 - List all authors'
-  puts '6 - List all music albums   12 - List all sources'
+  puts '1 - Add a new book          6 - List of games '
+  puts '2 - Add a new music album   7 - List of genres'
+  puts '3 - Add a new game          8 - List all labels'
+  puts '4 - List all books          9 - List all authors'
+  puts '5 - List all music albums '
+  puts '   '
   puts '--------------------------------------------------------'
   puts '0 - Exit'
 end
