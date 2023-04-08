4261f6384ce35719558afee3fc35e790e4041330
Aman Worku <amexworku2014@gmail.com>
2023-04-08 08:09:46 +0300

    Revert "update schema with game table"

--- schema/schema.sql
+++ schema/schema.sql
@@ -1,20 +1,4 @@
-CREATE DATABASE catalog;
-
-CREATE TABLE genres (
-    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    name VARCHAR(100) NOT NULL
-);
-
-CREATE TABLE authors (
-    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    first_name VARCHAR(100) NOT NULL,
-    last_name VARCHAR(100) NOT NULL
-);
-
-CREATE TABLE sources (
-    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    name VARCHAR(100) NOT NULL
-);
+CREATE DATABASE catalog
 
 CREATE TABLE items (
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -23,11 +7,11 @@ CREATE TABLE items (
     label_id INT REFERENCES labels(id),
     publish_date DATE NOT NULL,
     archived BOOLEAN NOT NULL
-);
+)
 
 CREATE TABLE music_albums (
     id INT GENERATED ALWAYS AS IDENTITY,
-    on_spotify BOOLEAN NOT NULL,
+    on_spotify BOOLEAN,
     publish_date DATE,
     archived BOOLEAN,
     PRIMARY KEY (id),
@@ -44,31 +28,10 @@ CREATE TABLE labels (
     PRIMARY KEY(id)
 );
 
-CREATE TABLE books (
-    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    publish_date DATE,
-    publisher VARCHAR(100) NOT NULL,
-    cover_state VARCHAR(100) NOT NULL,
-    label_id INT REFERENCES labels(id)
-);
-
-CREATE TABLE authors (
-    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    item_id INT REFERENCES items(id),
-    first_name VARCHAR(100) NOT NULL,
-    last_name VARCHAR(100) NOT NULL
-);
-
-CREATE TABLE games (
-    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    genre_id INTEGER REFERENCES genres(id),
-    label_id INTEGER REFERENCES labels(id),
-    author_id INTEGER REFERENCES authors(id),
-    source_id INTEGER REFERENCES sources(id),
-    archived BOOLEAN,
-    multiplayer VARCHAR(5),
-    last_played_at DATE,
-    FOREIGN KEY (genre_id) REFERENCES genres (id),
-    FOREIGN KEY (author_id) REFERENCES authors (id),
-    FOREIGN KEY (label_id) REFERENCES labels (id)
+CREATE TABLE book (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  publish_date DATE,
+  publisher VARCHAR(100) NOT NULL,
+  cover_state VARCHAR(100) NOT NULL,
+  label_id INT REFERENCES labels(id)
 );
